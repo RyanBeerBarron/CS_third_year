9d0eab9c0b37010076fc6d923378a3d5ce347842
Ryan Barron <barronry@itserv.scss.tcd.ie>
2018-09-19 13:53:08 +0100

    comp arch2

--- third_year/CS3021 Computer Architecture II/Labs/Tutorial 1/t1Test/t1Test/t1.asm
+++ third_year/CS3021 Computer Architecture II/Labs/Tutorial 1/t1Test/t1Test/t1.asm
@@ -5,3 +5,25 @@
 .code
 
 
+public	 min
+
+min:	push	ebp				;
+		mov		ebp, esp		;
+		mov		eax, [ebp+8]	; load first parameter in eax
+		mov		ebx, [ebp+12]	; load second parameter in ebx
+		mov		ecx, [ebp+16]	;
+		cmp		eax, ebx		;
+		jl		min0			;
+		cmp		ebx, ecx		;
+		jl		min1			;
+
+
+min0	cmp		eax, ecx
+		jl		min2
+
+
+
+
+
+
+min2	
