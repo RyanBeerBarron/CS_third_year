1f29205f3ab5b824bd0429a2d62bcdcc59bb5f55
Ryan <ryanbar1997@gmail.com>
2018-09-20 12:56:49 +0100

    stuff

--- third_year/CS3021 Computer Architecture II/Labs/Tutorial 1/t1Test/t1Test/t1.asm
+++ third_year/CS3021 Computer Architecture II/Labs/Tutorial 1/t1Test/t1Test/t1.asm
@@ -1,29 +1,36 @@
 686                                ; create 32 bit code
 .model flat, C                      ; 32 bit memory model
  option casemap:none                ; case sensitive
+.data
+public    g
+g    DWORD    4
+
 
 .code
-
-
 public	 min
 
 min:	push	ebp				;
 		mov		ebp, esp		;
+    sub esp, 4        ;
 		mov		eax, [ebp+8]	; load first parameter in eax
-		mov		ebx, [ebp+12]	; load second parameter in ebx
-		mov		ecx, [ebp+16]	;
-		cmp		eax, ebx		;
+		mov		ecx, [ebp+12]	; load second parameter in ecx
+		mov		edx, [ebp+16]	; load third parameter in edx
+    mov   [ebp-4], eax    ;
+    cmp		[ebp-4], ecx		;
 		jl		min0			;
-		cmp		ebx, ecx		;
-		jl		min1			;
+       mov   [ebp-4], ecx  ;
+min0   cmp		[ebp-4], edx	;
+		   jl		min1			;
+       mov [ebp-4], edx    ;
+min1   mov eax, [ebp-4]
+       mov esp, ebp
+       pop ebp
+       ret 0
+       
 
 
-min0	cmp		eax, ecx
-		jl		min2
 
 
 
 
-
-
-min2	
+min2
