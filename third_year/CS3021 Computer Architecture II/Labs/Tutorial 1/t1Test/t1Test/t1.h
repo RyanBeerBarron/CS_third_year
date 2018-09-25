01fd2b7b99da37753384083a30077b9282281e95
Ryan Barron <barronry@ITSERV.SCSS.TCD.IE>
2018-09-25 15:15:58 +0100

    done with comp arch2 tut1

--- third_year/CS3021 Computer Architecture II/Labs/Tutorial 1/t1Test/t1Test/t1.h
+++ third_year/CS3021 Computer Architecture II/Labs/Tutorial 1/t1Test/t1Test/t1.h
@@ -12,8 +12,8 @@
 // NB: "extern C" to avoid procedure name mangling by compiler
 //
 
-//extern "C" int g;
-//extern "C" int _cdecl min(int, int, int);   // _cdecl calling convention
-//extern "C" int _cdecl p(int, int, int, int);   // _cdecl calling convention
-//extern "C" int _cdecl gcd(int, int);
+extern "C" int g;
+extern "C" int _cdecl min(int, int, int);   // _cdecl calling convention
+extern "C" int _cdecl p(int, int, int, int);   // _cdecl calling convention
+extern "C" int _cdecl gcd(int, int);
 										// eof
