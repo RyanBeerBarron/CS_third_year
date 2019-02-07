e7634ed17aad303faf8e1ffe50a68511ec8c38d8
Ryan Barron <barronry@lg12-07.scss.tcd.ie>
2019-02-07 16:51:52 +0000

    Commented the MATLAB code

--- third_year/ST3009 Statistical Methods For Computer Science/dice_rolls.m
+++ third_year/ST3009 Statistical Methods For Computer Science/dice_rolls.m
@@ -1,11 +1,12 @@
-n = 100000;
+%Simulate 1000000 throws of 3 die and count the number of times there is a
+%2 in the resulting array of the random number generator. Divide the count
+%by n for the probability.
+n = 1000000;
 dice_throws = 3;
-a = randi(6,1,dice_throws);
 count = 0;
 for i = 1:n
-    b = randi(6,1,dice_throws);
-    a = cat(1,a,b);
-    c = ismember(2, b);
+    a = randi(6,1,dice_throws);
+    c = ismember(2, a);
     if c
         count = count + 1;
     end    
