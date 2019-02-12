4dab3a39413a2d2e7e24ebfe549b622d40ef88c0
Ryan <ryanbar1997@gmail.com>
2019-02-12 12:10:20 +0000

    Work on ai and stats

--- third_year/CS3061 AI/Assignment 1/incomplete.pl
+++ third_year/CS3061 AI/Assignment 1/incomplete.pl
@@ -8,7 +8,7 @@ makeKB(File):- open(File,read,Str),
    
 readK(Stream,[]):- at_end_of_stream(Stream),!.
 readK(Stream,[X|L]):- read(Stream,X),
-                      readK(Stream,L).
+                      readK(Stream,L).			
 
 reformat([],[]).
 reformat([end_of_file],[]) :- !.
@@ -24,31 +24,59 @@ initKB(File) :- retractall(kb(_)), makeKB(File).
 
 astar(Node,Path,Cost) :- kb(KB), astar(Node,Path,Cost,KB).
 
-goal([[]|_]).
+astar(Node,Path,Cost,KB) :- Frontier = [[[Node],0]], astarr(Frontier, Path, Cost, KB).
 
-arc([[[Head|Tail1]|History],Cost1], NewPathCost, KB) :- member([Head|Tail2], KB), append(Tail2,Tail1, NextProp), append([Head|Tail1], History, NewHistory),
-			length(Tail2, L), TempCost is L+1, Cost is TempCost + Cost1, NewPathCost = [[NextProp,NewHistory],Cost]. 
-
-add-to-frontier(Frontier, [], Frontier).
-add-to-frontier(Frontier, [PathCost|Rest], NewFrontier) :-
-	insert(PathCost, Frontier, TempFrontier),
-	add-to-frontier(TempFrontier, Rest, NewFrontier).
-
-insert(PathCost, [], [PathCost]).
-
-insert(PathCost, [PathCostH|PathCostT1], Res) :- PathCost = [_,Cost1], PathCostH = [_,Cost2],
-	(Cost1 =< Cost2, Res = [PathCost, PathCostH|PathCostT1] ;
-	Res = [PathCostH|PathCostT2], insert(PathCost, PathCostT1, PathCostT2)).
-	 
-
-astar(Node,Path,Cost,KB) :- Frontier = [[[Node],0]], astarr(Frontier, KB, End), End = [[Path,Cost]|_].
-
-astarr(Frontier, KB, End) :- Frontier = [H1|_], H1 = [Path|_], goal(Path), Frontier = End;
-	Frontier = [PathCost|PathCostRest],
-	PathCost = [Path,Cost], Path = [Current|History],
+astarr(Frontier, FinalPath, FinalCost, KB) :- Frontier = [PathCost|PathCostRest], PathCost = [Path,Cost], 
+	(goal(Path), FinalPath = Path, FinalCost = Cost ;
 	findall(PathCostL, arc(PathCost,PathCostL, KB), List),
 	add-to-frontier(PathCostRest, List, NewFrontier),
-	astarr(NewFrontier, KB, End).
+	astarr(NewFrontier, FinalPath, FinalCost, KB)). 
 	
+goal([[]|_]). 
 
+%Arc predicate, takes a PathCost pair, the knowledge base and the next PathCost pair connect to the first via the KB
+%Takes the head of the current node in the path of the Pathcost pair and looks in the KB for a proposition
+% or rule that starts with that head. With the body of the rule, create a new Node by appending it 
+%to the rest of the current Node and updates the path and cost to form the new pathcost pair.
+%In this case, the cost is just the basic arc cost of 1 for each new node.
+arc([[[HeadNode|TailNode]|PathHistory],Cost], NewPathCost, KB) :- 
+			member([HeadNode|RuleBody], KB), append(RuleBody,TailNode, NextNode),
+			length(RuleBody, L), LengthCost is L + 1
+			TotalCost is 1 + Cost, NewPathCost = [[NextNode,[HeadNode|TailNode]|PathHistory],TotalCost]. 
+
+%Arc predicate with Cost being Heuristic + arc Cost
+%arc([[[HeadNode|TailNode]|PathHistory],Cost], NewPathCost, KB) :- 
+%			member([HeadNode|RuleBody], KB), append(RuleBody,TailNode, NextNode),
+%			heuristic(RuleBody, H), NextCost is H+1, TotalCost is NextCost + Cost, 
+%			NewPathCost = [[NextNode,[HeadNode|TailNode]|PathHistory],TotalCost]. 
+
+
+%The heuristic for Astar with propositional knowledge base is just a function of the length of the current Node.
+heuristic(Node, H) :- length(Node, H).
 	
+%Add-to-frontier predicate takes the current set of frontier node, the new arc node and the resulting new frontier
+%from adding the first two variables.
+%The frontier is always sorted according to Astar cost function	
+add-to-frontier(Frontier, [], Frontier).
+add-to-frontier(Frontier, [PathCost|Rest], NewFrontier) :-
+	insert(PathCost, Frontier, FrontierResult),
+	add-to-frontier(FrontierResult, Rest, NewFrontier).
+
+
+%Insert predicate, takes a PathCost, a list of PathCost and the resulting list of PathCost after insertion
+%The list is always sorted according to the cost function used by Astar, uses the 'less-than' predicate to sort
+%the elements
+insert(PathCost, [], [PathCost]).
+insert(PathCost, [PathCostH|PathCostT1], Res) :-
+	(less-than(PathCost, PathCostH), Res = [PathCost, PathCostH|PathCostT1] ;
+	Res = [PathCostH|PathCostT2], insert(PathCost, PathCostT1, PathCostT2)).
+
+%Less-than predicate, takes two PathCost pair and succeeds
+%when the first Node1 has a smaller or equal value then the 
+%second Node2 according to the cost function used by Astar which
+%combines the cost of a Path and its heuristic
+%F(Node) = Cost(Node) + Heuristic(Node)
+less-than([[Node1|_],Cost1], [[Node2|_],Cost2]) :-
+	heuristic(Node1, H1), heuristic(Node2, H2),
+	F1 is Cost1 + H1, F2 is Cost2 + H2,
+	F1 =< F2.
