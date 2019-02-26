28a067140eab32272f5f433312efdede8d6486bf
Ryan Barron <barronry@lg12-27.scss.tcd.ie>
2019-02-26 15:41:42 +0000

    Fixed cdf.jpg

--- third_year/ST3009 Statistical Methods For Computer Science/pmf_cdf.m
+++ third_year/ST3009 Statistical Methods For Computer Science/pmf_cdf.m
@@ -3,7 +3,7 @@ x = [-3:2:3];
 
 y = [1/8, 3/8, 3/8, 1/8];
 z = [1/8, 4/8, 7/8, 1];
-bar(x,y)
+bar(x,z)
 
 xlabel('x')
-ylabel('$P(X{=}x)$','Interpreter','latex')
+ylabel('$P(X{\leq}x)$','Interpreter','latex')
