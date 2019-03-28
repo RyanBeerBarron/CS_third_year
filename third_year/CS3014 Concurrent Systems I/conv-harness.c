190e78717566a0db9d488e4a7f3f608775335b83
Ryan <ryanbar1997@gmail.com>
2019-03-28 14:21:32 +0000

    stoker

--- third_year/CS3014 Concurrent Systems I/conv-harness.c
+++ third_year/CS3014 Concurrent Systems I/conv-harness.c
@@ -421,6 +421,7 @@ inline static void team_conv(int16_t *** restrict image,  int16_t **** restrict 
   int threshhold = kernel_order * kernel_order * nchannels * nkernels;
   if( threshhold > 25000000 ) {
    //if(0) {
+    printf("1\n");
     #pragma omp parallel for collapse(3) 
       for ( m = 0; m < nkernels; m++ ) {
         for ( w = 0; w < width; w++ ) {
@@ -517,6 +518,7 @@ __m128i total = _mm_setzero_si128();
 
    //else if(1) {
   else if((width >= 256 && height >= 256 && kernel_order >= 3) || (width >= 64 && height >= 64 && nchannels >= 128)) {  
+    printf("2\n");
     #pragma omp parallel for collapse(3)
       for ( m = 0; m < nkernels; m++ ) {
         for ( w = 0; w < width; w++ ) {
@@ -563,6 +565,7 @@ __m128i total = _mm_setzero_si128();
   }
 
   else {
+    printf("3\n");
     for ( m = 0; m < nkernels; m++ ) {
         for ( w = 0; w < width; w++ ) {
           for ( h = 0; h < height; h++ ) {
