45832c4240cc8974009436a6e5c1ca3127bc571a
Ryan <ryanbar1997@gmail.com>
2019-04-03 20:34:21 +0100

    Polished ai assignment and had a look at telecommunication new project

--- third_year/CS3061 AI/Assignment 2/ai.pl
+++ third_year/CS3061 AI/Assignment 2/ai.pl
@@ -1,15 +1,16 @@
 :- dynamic q/5.
 :- dynamic v/4.
-:- dynamic v/3.
+:- dynamic gamma/1.
 
-prob(fit, exercise, fit, X) :- X is float(0.99) * float(0.9), asserta(prob(fit, exercise, fit, X) :- !).
-prob(fit, exercise, unfit, X) :- X is float(0.01) * float(0.9), asserta(prob(fit, exercise, unfit, X) :- !).
 
-prob(fit, relax, fit, X) :- X is float(0.7) * float(0.99), asserta(prob(fit, relax, fit, X) :- ! ).
-prob(fit, relax, unfit, X) :- X is float(0.3) * float(0.99), asserta(prob(fit, relax, unfit, X) :- ! ).
+prob(fit, exercise, fit, X) :- X is float(0.99) * float(0.9), asserta(prob(fit, exercise, fit, X) :- !), !.
+prob(fit, exercise, unfit, X) :- X is float(0.01) * float(0.9), asserta(prob(fit, exercise, unfit, X) :- !),! .
 
-prob(unfit, exercise, fit, X) :- X is float(0.2) * float(0.9), asserta(prob(unfit, exercise, fit, X) :- !).
-prob(unfit, exercise, unfit, X) :- X is float(0.8) * float(0.9), asserta(prob(unfit, exercise, unfit, X) :- !).
+prob(fit, relax, fit, X) :- X is float(0.7) * float(0.99), asserta(prob(fit, relax, fit, X) :- ! ), !.
+prob(fit, relax, unfit, X) :- X is float(0.3) * float(0.99), asserta(prob(fit, relax, unfit, X) :- ! ), !.
+
+prob(unfit, exercise, fit, X) :- X is float(0.2) * float(0.9), asserta(prob(unfit, exercise, fit, X) :- !), !.
+prob(unfit, exercise, unfit, X) :- X is float(0.8) * float(0.9), asserta(prob(unfit, exercise, unfit, X) :- !), !.
 
 prob(unfit, relax, fit, 0.0).
 prob(unfit, relax, unfit, 0.99).
@@ -21,8 +22,7 @@ prob(fit, relax, dead, 0.01).
 prob(unfit, relax, dead, 0.01).
 
 prob(dead, _, dead, 1.0).
-prob(dead, _, fit, 0.0).
-prob(dead, _, unfit, 0.0).
+prob(dead, _, X, 0.0) :- X \= dead.
 
 reward(fit, exercise, fit, 8).
 reward(fit, exercise, unfit, 8).
@@ -39,55 +39,42 @@ reward(unfit, relax, unfit, 5).
 reward(_, _, dead, 0).
 reward(dead, _, _, 0).
 
+
+
+show(N, State, Gamma) :- 	float(Gamma) > float(0.0), float(Gamma) < float(1.0),
+							retractall(gamma(_)), asserta(gamma(Gamma)),
+							show__(N, 0, State).
+
+show__(N, N, State) :-	gamma(Gamma),
+						( q(N, State, exercise, Qexercise, Gamma) ; q(N, State, exercise, Qexercise) ), !,
+						( q(N, State, relax, Qrelax, Gamma) ; q(N, State, relax, Qrelax) ), !,
+						format('n=~w, exer:~w relax:~w~n', [N, Qexercise, Qrelax]), !.
+
+show__(N, Acc, State) :- 	gamma(Gamma),
+							( q(Acc, State, exercise, Qexercise, Gamma) ; q(Acc, State, exercise, Qexercise) ), !,
+							( q(Acc, State, relax, Qrelax, Gamma) ; q(Acc, State, relax, Qrelax) ), !,
+							Newacc is Acc + 1,
+							format('n=~w, exer:~w relax:~w~n', [Acc, Qexercise, Qrelax]),
+							show__(N, Newacc, State).
+
 q(0, State, Action, Q):-prob(State, Action, fit, Prob1), reward(State, Action, fit, Reward1),
 						prob(State, Action, unfit, Prob2), reward(State, Action, unfit, Reward2),
 						prob(State, Action, dead, Prob3), reward(State, Action, dead, Reward3),
 						Q is Prob1 * Reward1 + Prob2 * Reward2 + Prob3 * Reward3,
-						asserta(q(0, State, Action, Q) :- !).
+						asserta(q(0, State, Action, Q) :- !), !.
 
-q(N, N, State, Action, Q, Gamma) :- q(N, State, Action, Q, Gamma).
+q(N, State, Action, Value) :- 	gamma(Gamma), M is N - 1,
+								( v(M, fit, Value1, Gamma); v(M, fit, Value1) ),
+								( v(M, unfit, Value2, Gamma); v(M, unfit, Value2) ),
+								( v(M, dead, Value3, Gamma); v(M, dead, Value3) ), !,
+								prob(State,Action,fit,Prob1),
+								prob(State, Action, unfit, Prob2),
+								prob(State, Action, dead, Prob3),
+								q(0, State, Action, Q0),
+								Value is Q0 + Gamma * (Prob1*Value1 + Prob2*Value2 + Prob3*Value3),
+								asserta(q(N, State, Action, Value, Gamma) :- !), !.
 
-q(N, Acc, State, Action, Q, Gamma) :- 	Acc < N, q(0, State, Action, Q0),
-								prob(State, Action, fit, Prob1), prob(State, Action, unfit, Prob2), prob(State, Action, dead, Prob3),
-								NewAcc is Acc + 1, v(Acc, fit, Value1, Gamma), v(Acc, unfit, Value2, Gamma), v(Acc, dead, Value3, Gamma),
-								TempQ is Q0 + Gamma * (Prob1*Value1 + Prob2*Value2 + Prob3*Value3),
-								asserta(q(NewAcc, State, Action, TempQ, Gamma) :- !), q(N, NewAcc, State, Action, Q, Gamma). 
-
-v(0, State, Value, _) :- 	(v(0, State, Value);	
-							(q(0, State, exercise, Q1), q(0, State, relax, Q2), max_list([Q1,Q2], Value),
-							asserta(v(0, State, Value) :- !))), !.
-
-v(N, State, Value, Gamma) :- 	(q(N, State, exercise, Q1, Gamma), ! ; q(N, 0, State, exercise, Q1, Gamma), !), 
-								(q(N, State, relax, Q2, Gamma), ! ; q(N, 0, State, relax, Q2, Gamma), !),
-								max_list([Q1,Q2],Value), asserta(v(N, State, Value, Gamma) :- !).
-
-show(N, State, Gamma) :- show(N, State, Gamma, _, _).
-
-show(N, State, Gamma, Qexercise, Qrelax) :- float(Gamma) > float(0.0), float(Gamma) < float(1.0), 
-											q(N, 0, State, exercise, Qexercise, Gamma), q(N, 0, State, relax, Qrelax, Gamma).
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+v(N, State, Value) :-	gamma(Gamma),
+						( q(N, State, exercise, Value1, Gamma) ;  q(N, State, exercise, Value1) ), !,
+						( q(N, State, relax, Value2, Gamma) ; q(N, State, relax, Value2) ), !,
+						max_list([Value1, Value2], Value), asserta( v(N,State,Value,Gamma) :- !), !.
